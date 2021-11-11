309217ab3a12d2ff0f87d4424c47b256de6d9633
fabio.vicini <fabio.vicini@polito.it>
2021-11-11 16:00:38 +0100

    add: new map test

--- test/Geometry/test_map.hpp
+++ test/Geometry/test_map.hpp
@@ -16,39 +16,69 @@ namespace GedimUnitTesting
 {
   TEST(TestQuadratureMap, TestMappingQuadrilateral)
   {
-    Eigen::MatrixXd vertices;
-    vertices.setZero(3, 4);
-    vertices.row(0) << 1.000000000000000e+00, 5.700000000000000e+00, 4.300000000000000e+00, 1.400000000000000e+00;
-    vertices.row(1) << 2.500000000000000e+00, -1.000000000000000e+00, 5.000000000000000e+00, 4.900000000000000e+00;
+    Eigen::MatrixXd referencePoints;
+    referencePoints.setZero(3, 9);
+    referencePoints.row(0)<< 1.127016653792583e-01, 5.000000000000000e-01, 8.872983346207417e-01, 1.127016653792583e-01, 5.000000000000000e-01, 8.872983346207417e-01, 1.127016653792583e-01, 5.000000000000000e-01, 8.872983346207417e-01;
+    referencePoints.row(1)<< 1.127016653792583e-01, 1.127016653792583e-01, 1.127016653792583e-01, 5.000000000000000e-01, 5.000000000000000e-01, 5.000000000000000e-01, 8.872983346207417e-01, 8.872983346207417e-01, 8.872983346207417e-01;
 
-    Gedim::MapQuadrilateral mapping;
+    Eigen::VectorXd referenceWeights(9);
+    referenceWeights<< 7.716049382716050e-02, 1.234567901234568e-01, 7.716049382716050e-02, 1.234567901234568e-01, 1.975308641975309e-01, 1.234567901234568e-01, 7.716049382716050e-02, 1.234567901234568e-01, 7.716049382716050e-02;
 
-    Eigen::MatrixXd points;
-    points.setZero(3, 9);
-    points.row(0)<< 1.127016653792583e-01, 5.000000000000000e-01, 8.872983346207417e-01, 1.127016653792583e-01, 5.000000000000000e-01, 8.872983346207417e-01, 1.127016653792583e-01, 5.000000000000000e-01, 8.872983346207417e-01;
-    points.row(1)<< 1.127016653792583e-01, 1.127016653792583e-01, 1.127016653792583e-01, 5.000000000000000e-01, 5.000000000000000e-01, 5.000000000000000e-01, 8.872983346207417e-01, 8.872983346207417e-01, 8.872983346207417e-01;
 
-    Eigen::MatrixXd mappedPoints = mapping.F(vertices,
-                                             points);
+    {
+      Eigen::MatrixXd vertices;
+      vertices.setZero(3, 4);
+      vertices.row(0) << 1.000000000000000e+00, 5.700000000000000e+00, 4.300000000000000e+00, 1.400000000000000e+00;
+      vertices.row(1) << 2.500000000000000e+00, -1.000000000000000e+00, 5.000000000000000e+00, 4.900000000000000e+00;
 
-    Eigen::MatrixXd expectedPoints;
-    expectedPoints.setZero(3, 9);
-    expectedPoints.row(0)<< 1.551915495751552e+00, 3.293649167310371e+00, 5.035382838869189e+00, 1.628266328441182e+00, 3.100000000000000e+00, 4.571733671558819e+00, 1.704617161130811e+00, 2.906350832689629e+00, 4.108084504248447e+00;
-    expectedPoints.row(1)<< 2.421754163448146e+00, 1.223346994592885e+00, 2.493982573762388e-02, 3.508407168855261e+00, 2.850000000000000e+00, 2.191592831144739e+00, 4.595060174262376e+00, 4.476653005407115e+00, 4.358245836551855e+00;
+      Gedim::MapQuadrilateral mapping;
 
-    ASSERT_TRUE((expectedPoints - mappedPoints).norm() < 1e-14);
+      Eigen::MatrixXd mappedPoints = mapping.F(vertices,
+                                               referencePoints);
 
-    Eigen::VectorXd weights(9);
-    weights<< 7.716049382716050e-02, 1.234567901234568e-01, 7.716049382716050e-02, 1.234567901234568e-01, 1.975308641975309e-01, 1.234567901234568e-01, 7.716049382716050e-02, 1.234567901234568e-01, 7.716049382716050e-02;
+      Eigen::MatrixXd expectedPoints;
+      expectedPoints.setZero(3, 9);
+      expectedPoints.row(0)<< 1.551915495751552e+00, 3.293649167310371e+00, 5.035382838869189e+00, 1.628266328441182e+00, 3.100000000000000e+00, 4.571733671558819e+00, 1.704617161130811e+00, 2.906350832689629e+00, 4.108084504248447e+00;
+      expectedPoints.row(1)<< 2.421754163448146e+00, 1.223346994592885e+00, 2.493982573762388e-02, 3.508407168855261e+00, 2.850000000000000e+00, 2.191592831144739e+00, 4.595060174262376e+00, 4.476653005407115e+00, 4.358245836551855e+00;
 
-    Eigen::VectorXd mappedWeights = weights.array() * mapping.DetJ(vertices,
-                                                                   points).array().abs();
+      ASSERT_TRUE((expectedPoints - mappedPoints).norm() < 1e-14);
 
-    Eigen::VectorXd expectedWeights(9);
-    expectedWeights<< 1.020658186245617e+00, 2.140844247829071e+00, 1.655397123540722e+00, 1.357640948929348e+00, 2.984691358024691e+00, 2.373223248601515e+00, 6.763929999160688e-01, 1.590019949701793e+00, 1.311131937211173e+00;
+      Eigen::VectorXd mappedWeights = referenceWeights.array() * mapping.DetJ(vertices,
+                                                                              referencePoints).array().abs();
 
-    ASSERT_TRUE((expectedWeights - mappedWeights).norm() < 1e-14);
-    ASSERT_TRUE(abs((mappedWeights).sum() - 1.511000000000000e+01) < 1e-14);
+      Eigen::VectorXd expectedWeights(9);
+      expectedWeights<< 1.020658186245617e+00, 2.140844247829071e+00, 1.655397123540722e+00, 1.357640948929348e+00, 2.984691358024691e+00, 2.373223248601515e+00, 6.763929999160688e-01, 1.590019949701793e+00, 1.311131937211173e+00;
+
+      ASSERT_TRUE((expectedWeights - mappedWeights).norm() < 1e-14);
+      ASSERT_DOUBLE_EQ(mappedWeights.sum(), 1.511000000000000e+01);
+    }
+
+    {
+      Eigen::MatrixXd vertices;
+      vertices.setZero(3, 4);
+      vertices.row(0) << -7.7312156205493587e-01,  6.3425787364898834e-01,  7.7312156205493587e-01, -6.3425787364898834e-01;
+      vertices.row(1) <<  6.3425787364898834e-01, -7.7312156205493587e-01, -6.3425787364898834e-01,  7.7312156205493587e-01;
+
+      Gedim::MapQuadrilateral mapping;
+      Eigen::MatrixXd mappedPoints = mapping.F(vertices,
+                                               referencePoints);
+
+      Eigen::MatrixXd expectedPoints;
+      expectedPoints.setZero(3, 9);
+      expectedPoints.row(0)<< -5.988573868865261e-01, -5.378167525891708e-02, 4.912940363686920e-01, -5.450757116276090e-01, 0, 5.450757116276090e-01, -4.912940363686920e-01, 5.378167525891706e-02, 5.988573868865261e-01;
+      expectedPoints.row(1)<< 4.912940363686920e-01, -5.378167525891708e-02, -5.988573868865261e-01, 5.450757116276090e-01, 0, -5.450757116276090e-01, 5.988573868865261e-01, 5.378167525891706e-02, -4.912940363686920e-01;
+
+      ASSERT_TRUE((expectedPoints - mappedPoints).norm() < 1e-14);
+
+      Eigen::VectorXd mappedWeights = referenceWeights.array() * mapping.DetJ(vertices,
+                                                                              referencePoints).array().abs();
+
+      Eigen::VectorXd expectedWeights(9);
+      expectedWeights<< 3.015955238094568e-02, 4.825528380951308e-02, 3.015955238094568e-02, 4.825528380951308e-02, 7.720845409522094e-02, 4.825528380951308e-02, 3.015955238094568e-02, 4.825528380951308e-02, 3.015955238094568e-02;
+
+      ASSERT_TRUE((expectedWeights - mappedWeights).norm() < 1e-14);
+      ASSERT_DOUBLE_EQ(mappedWeights.sum(), 3.908677988570559e-01);
+    }
   }
 
   TEST(TestQuadratureMap, TestMapTriangle)
