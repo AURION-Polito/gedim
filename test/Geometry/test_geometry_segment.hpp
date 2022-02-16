234bc2c377fc587c374acf35a80e8f23a0339cc2
fabio.vicini <fabio.vicini@polito.it>
2022-02-16 12:15:23 +0100

    add: test

--- test/Geometry/test_geometry_segment.hpp
+++ test/Geometry/test_geometry_segment.hpp
@@ -19,13 +19,64 @@ namespace GedimUnitTesting {
       Gedim::GeometryUtilitiesConfig geometryUtilityConfig;
       Gedim::GeometryUtilities geometryUtility(geometryUtilityConfig);
 
-      // check line passing in the origin
+      // check line passing in the origin positive
       {
         Eigen::Vector3d segmentOrigin(1.0, 1.0, 0.0);
         Eigen::Vector3d segmentEnd(2.0, 2.0, 0.0);
 
         const double expectedSlope = 1.0;
         const double expectedIntercept = 0.0;
+
+        ASSERT_TRUE(geometryUtility.Are1DValuesEqual(geometryUtility.SegmentSlope(segmentOrigin,
+                                                                                  segmentEnd),
+                                                     expectedSlope));
+
+        ASSERT_TRUE(geometryUtility.Are1DValuesEqual(geometryUtility.SegmentIntercept(segmentOrigin,
+                                                                                      segmentEnd),
+                                                     expectedIntercept));
+      }
+
+      // check line passing in the origin negative
+      {
+        Eigen::Vector3d segmentOrigin(-1.0, 1.0, 0.0);
+        Eigen::Vector3d segmentEnd(2.0, -2.0, 0.0);
+
+        const double expectedSlope = -1.0;
+        const double expectedIntercept = 0.0;
+
+        ASSERT_TRUE(geometryUtility.Are1DValuesEqual(geometryUtility.SegmentSlope(segmentOrigin,
+                                                                                  segmentEnd),
+                                                     expectedSlope));
+
+        ASSERT_TRUE(geometryUtility.Are1DValuesEqual(geometryUtility.SegmentIntercept(segmentOrigin,
+                                                                                      segmentEnd),
+                                                     expectedIntercept));
+      }
+
+      // check line not passing in the origin positive
+      {
+        Eigen::Vector3d segmentOrigin(0.0, 1.0, 0.0);
+        Eigen::Vector3d segmentEnd(-1.0, 0.0, 0.0);
+
+        const double expectedSlope = 1.0;
+        const double expectedIntercept = 1.0;
+
+        ASSERT_TRUE(geometryUtility.Are1DValuesEqual(geometryUtility.SegmentSlope(segmentOrigin,
+                                                                                  segmentEnd),
+                                                     expectedSlope));
+
+        ASSERT_TRUE(geometryUtility.Are1DValuesEqual(geometryUtility.SegmentIntercept(segmentOrigin,
+                                                                                      segmentEnd),
+                                                     expectedIntercept));
+      }
+
+      // check line not passing in the origin negtive
+      {
+        Eigen::Vector3d segmentOrigin(0.0, -1.0, 0.0);
+        Eigen::Vector3d segmentEnd(-1.0, 0.0, 0.0);
+
+        const double expectedSlope = -1.0;
+        const double expectedIntercept = -1.0;
 
         ASSERT_TRUE(geometryUtility.Are1DValuesEqual(geometryUtility.SegmentSlope(segmentOrigin,
                                                                                   segmentEnd),
