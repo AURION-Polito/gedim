063b087424fa58439483c42c937a23cd466b27b7
fabio.vicini <fabio.vicini@polito.it>
2022-06-16 17:03:26 +0200

    add: polyhedron cube creation

--- src/Geometry/GeometryUtilities_Polyhedron.cpp
+++ src/Geometry/GeometryUtilities_Polyhedron.cpp
@@ -83,4 +83,60 @@ namespace Gedim
     return tetrahedron;
   }
   // ***************************************************************************
+  GeometryUtilities::Polyhedron GeometryUtilities::CreateParallelepipedWithOrigin(const Eigen::Vector3d& origin,
+                                                                                  const Eigen::Vector3d& lengthVector,
+                                                                                  const Eigen::Vector3d& heightVector,
+                                                                                  const Eigen::Vector3d& widthVector) const
+  {
+    Gedim::GeometryUtilities::Polyhedron parallelpiped;
+
+    // create vertices
+    parallelpiped.Vertices.setZero(3, 8);
+    parallelpiped.Vertices.col(0)<< origin;
+    parallelpiped.Vertices.col(1)<< origin + lengthVector;
+    parallelpiped.Vertices.col(2)<< origin + lengthVector + widthVector;
+    parallelpiped.Vertices.col(3)<< origin + widthVector;
+    parallelpiped.Vertices.col(4)<< origin + heightVector;
+    parallelpiped.Vertices.col(5)<< origin + heightVector + lengthVector;
+    parallelpiped.Vertices.col(6)<< origin + heightVector + lengthVector + widthVector;
+    parallelpiped.Vertices.col(7)<< origin + heightVector + widthVector;
+
+    // create edges
+    parallelpiped.Edges.setZero(2, 12);
+    parallelpiped.Edges.col(0)<< 0, 1;
+    parallelpiped.Edges.col(1)<< 1, 2;
+    parallelpiped.Edges.col(2)<< 2, 3;
+    parallelpiped.Edges.col(3)<< 3, 0;
+    parallelpiped.Edges.col(4)<< 4, 5;
+    parallelpiped.Edges.col(5)<< 5, 6;
+    parallelpiped.Edges.col(6)<< 6, 7;
+    parallelpiped.Edges.col(7)<< 7, 4;
+    parallelpiped.Edges.col(8)<< 0, 4;
+    parallelpiped.Edges.col(9)<< 1, 5;
+    parallelpiped.Edges.col(10)<< 2, 6;
+    parallelpiped.Edges.col(11)<< 3, 7;
+
+    // create faces
+    parallelpiped.Faces.resize(6, MatrixXi::Zero(2, 4));
+    parallelpiped.Faces[0].row(0)<< 0, 1, 2, 3;
+    parallelpiped.Faces[0].row(1)<< 0, 1, 2, 3;
+
+    parallelpiped.Faces[1].row(0)<< 4, 5, 6, 7;
+    parallelpiped.Faces[1].row(1)<< 4, 5, 6, 7;
+
+    parallelpiped.Faces[2].row(0)<< 0, 3, 7, 4;
+    parallelpiped.Faces[2].row(1)<< 3, 11, 7, 8;
+
+    parallelpiped.Faces[3].row(0)<< 1, 2, 6, 5;
+    parallelpiped.Faces[3].row(1)<< 1, 10, 5, 9;
+
+    parallelpiped.Faces[4].row(0)<< 0, 1, 5, 4;
+    parallelpiped.Faces[4].row(1)<< 0, 9, 4, 8;
+
+    parallelpiped.Faces[5].row(0)<< 3, 2, 6, 7;
+    parallelpiped.Faces[5].row(1)<< 2, 10, 6, 11;
+
+    return parallelpiped;
+  }
+  // ***************************************************************************
 }
