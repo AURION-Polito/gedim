f37924efed0b8e0333dc790a0e88b6eda8bf4c3b
fabio.vicini <fabio.vicini@polito.it>
2022-06-24 15:29:52 +0200

    add: polyhedron tests

--- test/Geometry/test_geometry_polyhedron.hpp
+++ test/Geometry/test_geometry_polyhedron.hpp
@@ -10,7 +10,43 @@
 using namespace testing;
 using namespace std;
 
-namespace GedimUnitTesting {
+namespace GedimUnitTesting
+{
+
+  TEST(TestGeometryUtilities, TestPolyhedron_TestPolyhedronBarycenter)
+  {
+    try
+    {
+      Gedim::GeometryUtilitiesConfig geometryUtilityConfig;
+      Gedim::GeometryUtilities geometryUtility(geometryUtilityConfig);
+
+      // check cube barycenter
+      {
+        const Gedim::GeometryUtilities::Polyhedron cube = geometryUtility.CreateCubeWithOrigin(Eigen::Vector3d(0.0,0.0,0.0),
+                                                                                               Eigen::Vector3d(1.0,0.0,0.0),
+                                                                                               Eigen::Vector3d(0.0,0.0,1.0),
+                                                                                               Eigen::Vector3d(0.0,1.0,0.0));
+        ASSERT_TRUE(geometryUtility.PointsAreCoincident(geometryUtility.PolyhedronBarycenter(cube.Vertices),
+                                                        Eigen::Vector3d(0.5, 0.5, 0.5)));
+      }
+
+      // check tetrahedron barycenter
+      {
+        const Gedim::GeometryUtilities::Polyhedron tetrahedron = geometryUtility.CreateTetrahedronWithOrigin(Eigen::Vector3d(0.0,0.0,0.0),
+                                                                                                             Eigen::Vector3d(1.0,0.0,0.0),
+                                                                                                             Eigen::Vector3d(0.0,0.0,1.0),
+                                                                                                             Eigen::Vector3d(0.0,1.0,0.0));
+
+        ASSERT_TRUE(geometryUtility.PointsAreCoincident(geometryUtility.PolyhedronBarycenter(tetrahedron.Vertices),
+                                                        Eigen::Vector3d(0.25, 0.25, 0.25)));
+      }
+    }
+    catch (const exception& exception)
+    {
+      cerr<< exception.what()<< endl;
+      FAIL();
+    }
+  }
 }
 
 #endif // __TEST_GEOMETRY_POLYHEDRON_H
