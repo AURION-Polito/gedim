68d09e24c3bbe8f66d32505b67252a70d653b419
fabio.vicini <fabio.vicini@polito.it>
2022-12-13 10:19:45 +0100

    add: Pardiso cholesky

--- GeDiM/src/Algebra/Pardiso_CholeskySolver.hpp
+++ GeDiM/src/Algebra/Pardiso_CholeskySolver.hpp
@@ -1,8 +1,9 @@
-#ifndef __Pardiso_LUSolver_H
-#define __Pardiso_LUSolver_H
+#ifndef __Pardiso_CholeskySolver_H
+#define __Pardiso_CholeskySolver_H
 
 #include "ILinearSolver.hpp"
 #include "Eigen/Eigen"
+
 #include "Macro.hpp"
 
 #if ENABLE_MKL
@@ -11,21 +12,21 @@
 
 namespace Gedim
 {
-  /// \brief Pardiso LU Linear solver
+  /// \brief Pardiso Cholesky Linear solver
   template<typename Eigen_ArrayType = Eigen::VectorXd,
            typename Eigen_SparseArrayType = Eigen::SparseMatrix<double>>
-  class Pardiso_LUSolver final : public ILinearSolver
+  class Pardiso_CholeskySolver final : public ILinearSolver
   {
     private:
 #if ENABLE_MKL
-      Eigen::PardisoLU<Eigen_SparseArrayType> linearSolver; ///< The solver
+      Eigen::PardisoLDLT<Eigen_SparseArrayType, Eigen::Lower> linearSolver; ///< The solver
 #endif
-      const IArray* _rightHandSide; ///< The rightHandSide of the linear system
+      const IArray* _rightHandSide; ///< The rightHandSide of the linear syste
       IArray* _solution; ///< The solution of the linear syste
 
     public:
-      Pardiso_LUSolver();
-      ~Pardiso_LUSolver();
+      Pardiso_CholeskySolver();
+      ~Pardiso_CholeskySolver();
 
       void Initialize(const ISparseArray& matrix,
                       const IArray& rightHandSide,
@@ -34,10 +35,9 @@ namespace Gedim
       void Solve() const;
 
       void Initialize(const ISparseArray& matrix);
-
       void Solve(const IArray& rightHandSide,
                  IArray& solution) const;
   };
 }
 
-#endif // __Pardiso_LUSolver_H
+#endif // __Pardiso_CholeskySolver_H
