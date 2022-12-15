c82bfecc2b4a2d662d2d86af922bb0a3d4c1104a
fabio.vicini <fabio.vicini@polito.it>
2022-12-15 16:39:28 +0100

    fix: test

--- GeDiM/test/Mesh/test_conformMeshUtilities.hpp
+++ GeDiM/test/Mesh/test_conformMeshUtilities.hpp
@@ -5,16 +5,14 @@
 #include <gmock/gmock.h>
 #include <gmock/gmock-matchers.h>
 
+#include "GeometryUtilities.hpp"
+#include "MeshUtilities.hpp"
+
 #include "MeshMatrices.hpp"
-#include "MeshMatrices_2D_2Cells_Mock.hpp"
-#include "MeshMatrices_2D_4Cells_Mock.hpp"
 #include "MeshMatrices_2D_26Cells_Mock.hpp"
 #include "MeshMatricesDAO.hpp"
 
-#include "MeshDAOExporterToCsv.hpp"
-#include "MeshFromCsvUtilities.hpp"
-#include "MeshDAOImporterFromCsv.hpp"
-#include "FileTextReader.hpp"
+#include "ConformMeshUtilities.hpp"
 
 using namespace testing;
 using namespace std;
@@ -23,7 +21,50 @@ namespace GedimUnitTesting
 {
   TEST(TestConformMeshUtilities, TestConformMeshSingleSegment)
   {
-    
+    try
+    {
+      Gedim::GeometryUtilitiesConfig geometryUtilitiesConfig;
+      geometryUtilitiesConfig.Tolerance = 1e-14;
+      Gedim::GeometryUtilities geometryUtilities(geometryUtilitiesConfig);
+      Gedim::MeshUtilities meshUtilities;
+
+      GedimUnitTesting::MeshMatrices_2D_26Cells_Mock mesh;
+      Gedim::MeshMatricesDAO meshDAO(mesh.Mesh);
+
+      Gedim::ConformMeshUtilities conformMeshUtilities(geometryUtilities,
+                                                       meshUtilities);
+
+      const unsigned int numSegments = 2;
+      std::vector<Eigen::MatrixXd> segmentsVertices(numSegments);
+      std::vector<Eigen::Vector3d> segmentsTangent(numSegments);
+      std::vector<Eigen::Vector3d> segmentsBarycenter(numSegments);
+      std::vector<double> segmentsLength(numSegments);
+      std::vector<double> segmentsSquaredLength(numSegments);
+      vector<list<double>> segmentsAdditionalPoints(numSegments);
+
+      std::vector<Gedim::IntersectorMesh2DSegment::IntersectionMesh>& segmentsIntersectionMesh,
+      std::vector<std::vector<double> >& segmentsCurvilinearCoordinatesMesh,
+      vector<UnionMeshSegment::UnionMesh>& segmentsUnionMesh,
+      vector<ConformerMeshSegment::ConformMesh>& segmentsConformMesh,
+      vector<ConformerMeshPolygon::ConformMesh>& segmentsConformMeshInfo,
+      const ConformerMeshPolygon::ConformerMeshPolygonConfiguration::Types& conformDomainMeshType,
+      const ComputeDomainConformedMeshOptions& options
+
+      conformMeshUtilities.ComputeDomainConformedMesh(segmentsAdditionalPoints,
+                                                      segmentsVertices,
+                                                      segmentsTangent,
+                                                      segmentsBarycenter,
+                                                      segmentsLength,
+                                                      segmentsSquaredLength,
+                                                      meshDAO,
+                                                      );
+
+    }
+    catch (const exception& exception)
+    {
+      cerr<< exception.what()<< endl;
+      FAIL();
+    }
   }
 }
 
