9228dc00fb051f3da1f126981170b850f2ded47d
fabio.vicini <fabio.vicini@polito.it>
2023-04-11 16:07:25 +0200

    add: final test

---
+++ GeDiM/test/MpiTools/test_metisUtilities.hpp
@@ -0,0 +1,48 @@
+#ifndef __TEST_METISUTILITIES_H
+#define __TEST_METISUTILITIES_H
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <gmock/gmock-matchers.h>
+
+#include "Macro.hpp"
+#include "MetisUtilities.hpp"
+#include "IOUtilities.hpp"
+
+namespace UnitTesting
+{
+  TEST(TestMetisUtilities, TestNetworkPartition)
+  {
+    Gedim::MetisUtilities metisUtilities;
+
+    const unsigned int numberVertices = 6;
+    Eigen::MatrixXi edges(2, 6);
+    edges.col(0)<< 0, 1;
+    edges.col(1)<< 1, 2;
+    edges.col(2)<< 1, 3;
+    edges.col(3)<< 1, 5;
+    edges.col(4)<< 2, 4;
+    edges.col(5)<< 2, 5;
+
+    const Gedim::MetisUtilities::NetworkAdjacency adjacency = metisUtilities.GraphToAdjacency(numberVertices,
+                                                                                              edges,
+                                                                                              true);
+
+    ASSERT_EQ(std::vector<unsigned int>({ 0,1,5,8,9,10,12 }), adjacency.AdjacencyRows);
+    ASSERT_EQ(std::vector<unsigned int>({ 1,0,2,3,5,1,4,5,1,2,1,2 }), adjacency.AdjacencyCols);
+
+    Gedim::MetisUtilities::NetworkPartitionOptions partitionOptions;
+    partitionOptions.PartitionType = Gedim::MetisUtilities::NetworkPartitionOptions::PartitionTypes::CutBalancing;
+    partitionOptions.MasterWeight = 100;
+    partitionOptions.NumberOfParts = 2;
+    partitionOptions.NodeWeights = {};
+    partitionOptions.EdgeWeights = {};
+
+    const std::vector<unsigned int> partition = metisUtilities.NetworkPartition(partitionOptions,
+                                                                                adjacency);
+
+    ASSERT_EQ(std::vector<unsigned int>({ 1, 1, 0, 1, 0, 0 }), partition);
+  }
+}
+
+#endif // __TEST_METISUTILITIES_H
