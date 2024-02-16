ff60246d0cd399c07d9f554bc0b8f53f91b990bf
fabio.vicini <fabio.vicini@polito.it>
2024-02-16 15:31:53 +0100

    fix

--- GeDiM/test/IO/test_VTKUtilities.hpp
+++ GeDiM/test/IO/test_VTKUtilities.hpp
@@ -79,6 +79,21 @@ namespace GedimUnitTesting
       data[1] = 20 + g;
       data[2] = 30 + g;
       data[3] = 40 + g;
+      vector<double> u(3 * 4);
+      for (unsigned int p = 0; p < 4; p++)
+      {
+        u[3 * p] = (p + 1) + g + 1;
+        u[3 * p + 1] = (p + 1) + g + 2;
+        u[3 * p + 2] = (p + 1) + g + 3;
+      }
+
+      vector<double> w(3 * 4);
+      for (unsigned int p = 0; p < 4; p++)
+      {
+        w[3 * p] = (p + 1) + g + 1;
+        w[3 * p + 1] = (p + 1) + g + 2;
+        w[3 * p + 2] = (p + 1) + g + 3;
+      }
 
       vtpUtilities.AddPoints(geometry,
                              {
@@ -93,6 +108,18 @@ namespace GedimUnitTesting
                                  Gedim::VTPProperty::Formats::Points,
                                  static_cast<unsigned int>(data.size()),
                                  data.data()
+                               },
+                               {
+                                 "u",
+                                 Gedim::VTPProperty::Formats::PointsArray,
+                                 static_cast<unsigned int>(u.size()),
+                                 u.data()
+                               },
+                               {
+                                 "w",
+                                 Gedim::VTPProperty::Formats::CellsArray,
+                                 static_cast<unsigned int>(w.size()),
+                                 w.data()
                                }
                              });
     }
