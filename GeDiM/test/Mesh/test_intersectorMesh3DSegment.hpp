248d6ca3304fa44b97712ac274f9ba94e6985a20
fabio.vicini <fabio.vicini@polito.it>
2024-05-15 15:45:57 +0200

    add: last cell

--- GeDiM/test/Mesh/test_intersectorMesh3DSegment.hpp
+++ GeDiM/test/Mesh/test_intersectorMesh3DSegment.hpp
@@ -277,12 +277,17 @@ namespace GedimUnitTesting
     const Gedim::MeshUtilities::MeshGeometricData3D mesh3D_geometricData = meshUtilities.FillMesh3DGeometricData(geometryUtilities,
                                                                                                                  mesh3D);
 
-    const Eigen::Vector3d segmentOrigin(3.1628787878787878e-01,
-                                        3.1628787878787878e-01,
-                                        3.1628787878787878e-01);
-    const Eigen::Vector3d segmentEnd(2.9578836930455638e-01,
-                                     6.7768285371702630e-01,
-                                     3.2231714628297359e-01);
+    const Eigen::Vector3d segment_test_Origin(3.1628787878787878e-01,
+                                              3.1628787878787878e-01,
+                                              3.1628787878787878e-01);
+    const Eigen::Vector3d segment_test_End(2.9578836930455638e-01,
+                                           6.7768285371702630e-01,
+                                           3.2231714628297359e-01);
+    const Eigen::Vector3d segment_test_Tangent = geometryUtilities.SegmentTangent(segment_test_Origin,
+                                                                                  segment_test_End);
+
+    const Eigen::Vector3d segmentOrigin = segment_test_Origin - 0.5 * segment_test_Tangent;
+    const Eigen::Vector3d segmentEnd = segment_test_Origin + 1.1 * segment_test_Tangent;
     const Eigen::Vector3d segmentTangent = geometryUtilities.SegmentTangent(segmentOrigin,
                                                                             segmentEnd);
     const Eigen::Vector3d segmentBarycenter = geometryUtilities.SegmentBarycenter(segmentOrigin,
@@ -318,13 +323,19 @@ namespace GedimUnitTesting
 
     std::cout<< Gedim::IntersectorMesh3DSegment::ToString(intersections)<< std::endl;
 
-    EXPECT_EQ(intersections.Points.size(), 2);
-    EXPECT_EQ(intersections.Points[0].CurvilinearCoordinate, 0.0);
-    EXPECT_EQ(intersections.Points[1].CurvilinearCoordinate, 1.0);
-    EXPECT_EQ(intersections.Points[0].Cell3DIds, std::vector<unsigned int>({ 0,18,21,25,40,60,62 }));
-    EXPECT_EQ(intersections.Points[1].Cell3DIds, std::vector<unsigned int>({ 1,17,27,50,52,62 }));
-    EXPECT_EQ(intersections.Segments.size(), 1);
-    EXPECT_EQ(intersections.Segments[0].Cell3DIds, std::vector<unsigned int>({ 62 }));
+    EXPECT_EQ(intersections.Points.size(), 4);
+    EXPECT_EQ(intersections.Points[0].CurvilinearCoordinate, 0.0000000000000000e+00);
+    EXPECT_EQ(intersections.Points[1].CurvilinearCoordinate, 3.1249999999999967e-01);
+    EXPECT_EQ(intersections.Points[2].CurvilinearCoordinate, 9.3749999999999989e-01);
+    EXPECT_EQ(intersections.Points[3].CurvilinearCoordinate, 1.0000000000000000e+00);
+    EXPECT_EQ(intersections.Points[0].Cell3DIds, std::vector<unsigned int>({ 81 }));
+    EXPECT_EQ(intersections.Points[1].Cell3DIds, std::vector<unsigned int>({ 12,59,69,81,111,116,148,152,156,168,196,225,246,250,262,263,270,276,279,280,281,288,298,305,311,312,327,328 }));
+    EXPECT_EQ(intersections.Points[2].Cell3DIds, std::vector<unsigned int>({ 1,127,141,154,173,181,188,191,200,231,246,248,250,272,273,277,278,280,281,282,298,300,303,306,314,315,317,323 }));
+    EXPECT_EQ(intersections.Points[3].Cell3DIds, std::vector<unsigned int>({ 323 }));
+    EXPECT_EQ(intersections.Segments.size(), 3);
+    EXPECT_EQ(intersections.Segments[0].Cell3DIds, std::vector<unsigned int>({ 81 }));
+    EXPECT_EQ(intersections.Segments[1].Cell3DIds, std::vector<unsigned int>({ 246,250,280,281,298 }));
+    EXPECT_EQ(intersections.Segments[2].Cell3DIds, std::vector<unsigned int>({ 323 }));
 
     Gedim::MeshMatrices mesh_1D_data;
     Gedim::MeshMatricesDAO mesh_1D(mesh_1D_data);
