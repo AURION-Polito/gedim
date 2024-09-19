2deafeb79dab4e0eaa2d4777e922fb82c9e83448
fabio.vicini <fabio.vicini@polito.it>
2024-09-19 10:53:22 +0200

    still

--- GeDiM/src/Mesh/MeshUtilities_Intersect_mesh_polyhedron.cpp
+++ GeDiM/src/Mesh/MeshUtilities_Intersect_mesh_polyhedron.cpp
@@ -17,9 +17,19 @@ namespace Gedim
                                                   const Eigen::MatrixXd& polyhedron_boudingBox,
                                                   const IMeshDAO& mesh) const
   {
-    std::list<Eigen::Vector3d> intersections_coordinates;
+    struct Intersection final
+    {
+        Intersect_mesh_polyhedron_result::Polyhedron_Intersection::Types Type;
+        unsigned int Geometry_index;
+        std::list<unsigned int> Cell0Ds_index;
+        std::list<unsigned int> Cell1Ds_index;
+        std::list<unsigned int> Cell2Ds_index;
+        std::list<unsigned int> Cell3Ds_index;
+        Eigen::Vector3d Intersection_coordinates;
+    };
+
+    std::list<Intersection> intersections;
     Intersect_mesh_polyhedron_result::Mesh_Intersections mesh_intersections;
-    std::list<Intersect_mesh_polyhedron_result::Polyhedron_Intersection> polyhedron_intersections;
 
     for (unsigned int p = 0; p < mesh.Cell0DTotalNumber(); ++p)
     {
@@ -47,16 +57,55 @@ namespace Gedim
         case GeometryUtilities::PointPolyhedronPositionResult::Types::Outside:
           break;
         case GeometryUtilities::PointPolyhedronPositionResult::Types::Inside:
+        {
+          unsigned int new_intersection_index = intersections.size();
+          Intersection new_intersection;
+          new_intersection.Type = Intersect_mesh_polyhedron_result::Polyhedron_Intersection::Types::Polyhedron;
+          new_intersection.Geometry_index = 0;
+          new_intersection.Intersection_coordinates = cell0D_coordinates;
+          new_intersection.Cell0Ds_index = { p };
+          mesh_intersections.Cell0Ds_intersections.insert(std::make_pair(p, new_intersection_index));
+        }
           break;
-
+        case GeometryUtilities::PointPolyhedronPositionResult::Types::BorderFace:
+        {
+          unsigned int new_intersection_index = intersections.size();
+          Intersection new_intersection;
+          new_intersection.Type = Intersect_mesh_polyhedron_result::Polyhedron_Intersection::Types::Face;
+          new_intersection.Geometry_index = cell0D_polyhedron_position.BorderIndex;
+          new_intersection.Intersection_coordinates = cell0D_coordinates;
+          new_intersection.Cell0Ds_index = { p };
+          mesh_intersections.Cell0Ds_intersections.insert(std::make_pair(p, new_intersection_index));
+        }
+          break;
         case GeometryUtilities::PointPolyhedronPositionResult::Types::BorderEdge:
+        {
+          unsigned int new_intersection_index = intersections.size();
+          Intersection new_intersection;
+          new_intersection.Type = Intersect_mesh_polyhedron_result::Polyhedron_Intersection::Types::Edge;
+          new_intersection.Geometry_index = cell0D_polyhedron_position.BorderIndex;
+          new_intersection.Intersection_coordinates = cell0D_coordinates;
+          new_intersection.Cell0Ds_index = { p };
+          mesh_intersections.Cell0Ds_intersections.insert(std::make_pair(p, new_intersection_index));
+        }
+          break;
+        case GeometryUtilities::PointPolyhedronPositionResult::Types::BorderVertex:
+        {
+          unsigned int new_intersection_index = intersections.size();
+          Intersection new_intersection;
+          new_intersection.Type = Intersect_mesh_polyhedron_result::Polyhedron_Intersection::Types::Vertex;
+          new_intersection.Geometry_index = cell0D_polyhedron_position.BorderIndex;
+          new_intersection.Intersection_coordinates = cell0D_coordinates;
+          new_intersection.Cell0Ds_index = { p };
+          mesh_intersections.Cell0Ds_intersections.insert(std::make_pair(p, new_intersection_index));
+        }
           break;
         default:
           throw std::runtime_error("Unexpected cell0D intersection");
       }
     }
 
-    if (intersections_coordinates.empty())
+    if (intersections.empty())
     {
       return
       {
@@ -67,14 +116,32 @@ namespace Gedim
       };
     }
 
-    return
+    Intersect_mesh_polyhedron_result result;
+    result.Type = Intersect_mesh_polyhedron_result::Types::Vertices;
+    result.Mesh_intersections = mesh_intersections;
+
+    result.Intersections_Coordinates.resize(3, intersections.size());
+    result.Polyhedron_intersections.resize(intersections.size());
+
+    unsigned int i = 0;
+    for (const auto& intersection : intersections)
     {
-      Intersect_mesh_polyhedron_result::Types::Vertices,
-          Eigen_Utilities::CoordinatesToMatrix(intersections_coordinates),
-          std::vector<Intersect_mesh_polyhedron_result::Polyhedron_Intersection>(polyhedron_intersections.begin(),
-                                                                                 polyhedron_intersections.end()),
-          mesh_intersections
-    };
+      result.Intersections_Coordinates.col(i)<< intersection.Intersection_coordinates;
+
+      result.Polyhedron_intersections[i].Type = intersection.Type;
+      result.Polyhedron_intersections[i].Cell0Ds_index = std::vector<unsigned int>(intersection.Cell0Ds_index.begin(),
+                                                                                   intersection.Cell0Ds_index.end());
+      result.Polyhedron_intersections[i].Cell1Ds_index = std::vector<unsigned int>(intersection.Cell1Ds_index.begin(),
+                                                                                   intersection.Cell1Ds_index.end());
+      result.Polyhedron_intersections[i].Cell2Ds_index = std::vector<unsigned int>(intersection.Cell2Ds_index.begin(),
+                                                                                   intersection.Cell2Ds_index.end());
+      result.Polyhedron_intersections[i].Cell3Ds_index = std::vector<unsigned int>(intersection.Cell3Ds_index.begin(),
+                                                                                   intersection.Cell3Ds_index.end());
+
+      i++;
+    }
+
+    return result;
   }
   // ***************************************************************************
 }
