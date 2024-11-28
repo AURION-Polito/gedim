20ea652e4b00441562527159aa7c656c361f653c
gioanateora <gioana.teora@polito.it>
2024-11-28 15:33:22 +0100

    fix in PETSc

--- GeDiM/src/Algebra/PETSc_KSPSolver.cpp
+++ GeDiM/src/Algebra/PETSc_KSPSolver.cpp
@@ -7,99 +7,99 @@
 
 namespace Gedim
 {
-  // ***************************************************************************
-  template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPCG, PETSc_Preconditioners::PETSc_DEFAULT>;
-  template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPCG, PETSc_Preconditioners::PETSc_PCNONE>;
-  template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPCG, PETSc_Preconditioners::PETSc_PCJACOBI>;
-  template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPCG, PETSc_Preconditioners::PETSc_PCILU>;
-  template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPCG, PETSc_Preconditioners::PETSc_PCFIELDSPLIT>;
-  template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPBICG, PETSc_Preconditioners::PETSc_DEFAULT>;
-  template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPBICG, PETSc_Preconditioners::PETSc_PCNONE>;
-  template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPBICG, PETSc_Preconditioners::PETSc_PCJACOBI>;
-  template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPBICG, PETSc_Preconditioners::PETSc_PCILU>;
-  template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPBICG, PETSc_Preconditioners::PETSc_PCFIELDSPLIT>;
-  template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPGMRES, PETSc_Preconditioners::PETSc_DEFAULT>;
-  template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPGMRES, PETSc_Preconditioners::PETSc_PCNONE>;
-  template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPGMRES, PETSc_Preconditioners::PETSc_PCJACOBI>;
-  template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPGMRES, PETSc_Preconditioners::PETSc_PCILU>;
-  template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPGMRES, PETSc_Preconditioners::PETSc_PCFIELDSPLIT>;
-  // ***************************************************************************
-  template<typename PETSc_ArrayType,
-           typename PETSc_SparseArrayType,
-           PETSc_SolverTypes PETSc_SolverType,
-           PETSc_Preconditioners PETSc_Preconditioner>
-  PETSc_KSPSolver<
-  PETSc_ArrayType,
-  PETSc_SparseArrayType,
-  PETSc_SolverType,
-  PETSc_Preconditioner>::PETSc_KSPSolver()
-  {
+// ***************************************************************************
+template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPCG, PETSc_Preconditioners::PETSc_DEFAULT>;
+template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPCG, PETSc_Preconditioners::PETSc_PCNONE>;
+template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPCG, PETSc_Preconditioners::PETSc_PCJACOBI>;
+template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPCG, PETSc_Preconditioners::PETSc_PCILU>;
+template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPCG, PETSc_Preconditioners::PETSc_PCFIELDSPLIT>;
+template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPBICG, PETSc_Preconditioners::PETSc_DEFAULT>;
+template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPBICG, PETSc_Preconditioners::PETSc_PCNONE>;
+template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPBICG, PETSc_Preconditioners::PETSc_PCJACOBI>;
+template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPBICG, PETSc_Preconditioners::PETSc_PCILU>;
+template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPBICG, PETSc_Preconditioners::PETSc_PCFIELDSPLIT>;
+template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPGMRES, PETSc_Preconditioners::PETSc_DEFAULT>;
+template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPGMRES, PETSc_Preconditioners::PETSc_PCNONE>;
+template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPGMRES, PETSc_Preconditioners::PETSc_PCJACOBI>;
+template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPGMRES, PETSc_Preconditioners::PETSc_PCILU>;
+template class PETSc_KSPSolver<Vec, Mat, PETSc_SolverTypes::PETSc_KSPGMRES, PETSc_Preconditioners::PETSc_PCFIELDSPLIT>;
+// ***************************************************************************
+template<typename PETSc_ArrayType,
+         typename PETSc_SparseArrayType,
+         PETSc_SolverTypes PETSc_SolverType,
+         PETSc_Preconditioners PETSc_Preconditioner>
+PETSc_KSPSolver<
+    PETSc_ArrayType,
+    PETSc_SparseArrayType,
+    PETSc_SolverType,
+    PETSc_Preconditioner>::PETSc_KSPSolver()
+{
     _rightHandSide = nullptr;
     _solution = nullptr;
-  }
-  template<typename PETSc_ArrayType,
-           typename PETSc_SparseArrayType,
-           PETSc_SolverTypes PETSc_SolverType,
-           PETSc_Preconditioners PETSc_Preconditioner>
-  PETSc_KSPSolver<
-  PETSc_ArrayType,
-  PETSc_SparseArrayType,
-  PETSc_SolverType,
-  PETSc_Preconditioner>::~PETSc_KSPSolver()
-  {
+}
+template<typename PETSc_ArrayType,
+         typename PETSc_SparseArrayType,
+         PETSc_SolverTypes PETSc_SolverType,
+         PETSc_Preconditioners PETSc_Preconditioner>
+PETSc_KSPSolver<
+    PETSc_ArrayType,
+    PETSc_SparseArrayType,
+    PETSc_SolverType,
+    PETSc_Preconditioner>::~PETSc_KSPSolver()
+{
     _rightHandSide = nullptr;
     _solution = nullptr;
-  }
-  // ***************************************************************************
-  template<typename PETSc_ArrayType,
-           typename PETSc_SparseArrayType,
-           PETSc_SolverTypes PETSc_SolverType,
-           PETSc_Preconditioners PETSc_Preconditioner>
-  void PETSc_KSPSolver<
-  PETSc_ArrayType,
-  PETSc_SparseArrayType,
-  PETSc_SolverType,
-  PETSc_Preconditioner>::Initialize(const ISparseArray& matrix,
-                                    const IArray& rightHandSide,
-                                    IArray& solution,
-                                    const Configuration& configuration)
-  {
+}
+// ***************************************************************************
+template<typename PETSc_ArrayType,
+         typename PETSc_SparseArrayType,
+         PETSc_SolverTypes PETSc_SolverType,
+         PETSc_Preconditioners PETSc_Preconditioner>
+void PETSc_KSPSolver<
+    PETSc_ArrayType,
+    PETSc_SparseArrayType,
+    PETSc_SolverType,
+    PETSc_Preconditioner>::Initialize(const ISparseArray& matrix,
+                                      const IArray& rightHandSide,
+                                      IArray& solution,
+                                      const Configuration& configuration)
+{
     _rightHandSide = &rightHandSide;
     _solution = &solution;
 
     Initialize(matrix,
                configuration);
-  }
-  // ***************************************************************************
-  template<typename PETSc_ArrayType,
-           typename PETSc_SparseArrayType,
-           PETSc_SolverTypes PETSc_SolverType,
-           PETSc_Preconditioners PETSc_Preconditioner>
-  ILinearSolver::SolutionInfo PETSc_KSPSolver<
-  PETSc_ArrayType,
-  PETSc_SparseArrayType,
-  PETSc_SolverType,
-  PETSc_Preconditioner>::Solve() const
-  {
+}
+// ***************************************************************************
+template<typename PETSc_ArrayType,
+         typename PETSc_SparseArrayType,
+         PETSc_SolverTypes PETSc_SolverType,
+         PETSc_Preconditioners PETSc_Preconditioner>
+ILinearSolver::SolutionInfo PETSc_KSPSolver<
+    PETSc_ArrayType,
+    PETSc_SparseArrayType,
+    PETSc_SolverType,
+    PETSc_Preconditioner>::Solve() const
+{
     if (_rightHandSide == nullptr ||
         _solution == nullptr)
-      throw runtime_error("No initialization found");
+        throw std::runtime_error("No initialization found");
 
     return Solve(*_rightHandSide,
                  *_solution);
-  }
-  // ***************************************************************************
-  template<typename PETSc_ArrayType,
-           typename PETSc_SparseArrayType,
-           PETSc_SolverTypes PETSc_SolverType,
-           PETSc_Preconditioners PETSc_Preconditioner>
-  void PETSc_KSPSolver<
-  PETSc_ArrayType,
-  PETSc_SparseArrayType,
-  PETSc_SolverType,
-  PETSc_Preconditioner>::Initialize(const ISparseArray& matrix,
-                                    const Configuration& configuration)
-  {
+}
+// ***************************************************************************
+template<typename PETSc_ArrayType,
+         typename PETSc_SparseArrayType,
+         PETSc_SolverTypes PETSc_SolverType,
+         PETSc_Preconditioners PETSc_Preconditioner>
+void PETSc_KSPSolver<
+    PETSc_ArrayType,
+    PETSc_SparseArrayType,
+    PETSc_SolverType,
+    PETSc_Preconditioner>::Initialize(const ISparseArray& matrix,
+                                      const Configuration& configuration)
+{
     _config = configuration;
 
     const auto& A_PETSc_Array = static_cast<const PETSc_SparseArray<PETSc_ArrayType, PETSc_SparseArrayType>&>(matrix);
@@ -110,41 +110,41 @@ namespace Gedim
 
     switch (PETSc_SolverType)
     {
-      case PETSc_SolverTypes::PETSc_KSPCG:
+    case PETSc_SolverTypes::PETSc_KSPCG:
         KSPSetType(linearSolver, KSPCG);
         break;
-      case PETSc_SolverTypes::PETSc_KSPBICG:
+    case PETSc_SolverTypes::PETSc_KSPBICG:
         KSPSetType(linearSolver, KSPBICG);
         break;
-      case PETSc_SolverTypes::PETSc_KSPGMRES:
+    case PETSc_SolverTypes::PETSc_KSPGMRES:
         KSPSetType(linearSolver, KSPGMRES);
         break;
-      default:
+    default:
         throw std::runtime_error("Unsopported PETSc solver type");
     }
 
     KSPGetPC(linearSolver, &preconditioner);
     switch (PETSc_Preconditioner)
     {
-      case PETSc_Preconditioners::PETSc_DEFAULT:
+    case PETSc_Preconditioners::PETSc_DEFAULT:
         break;
-      case PETSc_Preconditioners::PETSc_PCNONE:
+    case PETSc_Preconditioners::PETSc_PCNONE:
         PCSetType(preconditioner, PCNONE);
         PCSetFromOptions(preconditioner);
         break;
-      case PETSc_Preconditioners::PETSc_PCILU:
+    case PETSc_Preconditioners::PETSc_PCILU:
         PCSetType(preconditioner, PCILU);
         PCSetFromOptions(preconditioner);
         break;
-      case PETSc_Preconditioners::PETSc_PCJACOBI:
+    case PETSc_Preconditioners::PETSc_PCJACOBI:
         PCSetType(preconditioner, PCJACOBI);
         PCSetFromOptions(preconditioner);
         break;
-      case PETSc_Preconditioners::PETSc_PCFIELDSPLIT:
+    case PETSc_Preconditioners::PETSc_PCFIELDSPLIT:
         PCSetType(preconditioner, PCFIELDSPLIT);
         PCSetFromOptions(preconditioner);
         break;
-      default:
+    default:
         throw std::runtime_error("Unsopported PETSc preconditioner type");
     }
 
@@ -156,19 +156,19 @@ namespace Gedim
 
     CHKERRABORT(PETSC_COMM_WORLD,
                 KSPSetFromOptions(linearSolver));
-  }
-  // ***************************************************************************
-  template<typename PETSc_ArrayType,
-           typename PETSc_SparseArrayType,
-           PETSc_SolverTypes PETSc_SolverType,
-           PETSc_Preconditioners PETSc_Preconditioner>
-  ILinearSolver::SolutionInfo PETSc_KSPSolver<
-  PETSc_ArrayType,
-  PETSc_SparseArrayType,
-  PETSc_SolverType,
-  PETSc_Preconditioner>::Solve(const IArray& rightHandSide,
-                               IArray& solution) const
-  {
+}
+// ***************************************************************************
+template<typename PETSc_ArrayType,
+         typename PETSc_SparseArrayType,
+         PETSc_SolverTypes PETSc_SolverType,
+         PETSc_Preconditioners PETSc_Preconditioner>
+ILinearSolver::SolutionInfo PETSc_KSPSolver<
+    PETSc_ArrayType,
+    PETSc_SparseArrayType,
+    PETSc_SolverType,
+    PETSc_Preconditioner>::Solve(const IArray& rightHandSide,
+                                 IArray& solution) const
+{
     const auto& rhs_PETSc_Array = static_cast<const PETSc_Array<PETSc_ArrayType, PETSc_SparseArrayType>&>(rightHandSide);
     const PETSc_ArrayType& rhs_PETSc = static_cast<const PETSc_ArrayType&>(rhs_PETSc_Array);
 
@@ -182,8 +182,8 @@ namespace Gedim
     KSPGetConvergedReason(linearSolver, &reason);
     if (reason < 0)
     {
-      throw std::runtime_error("KSP did not converge.\n");
-      PetscPrintf(PETSC_COMM_WORLD, "KSP did not converge.\n");
+        throw std::runtime_error("KSP did not converge.\n");
+        PetscPrintf(PETSC_COMM_WORLD, "KSP did not converge.\n");
     }
 
     PetscInt iterations;
@@ -193,11 +193,11 @@ namespace Gedim
     KSPGetResidualNorm(linearSolver, &residual);
 
     return
-    {
-      static_cast<unsigned int>(iterations),
-          static_cast<double>(residual)
-    };
-  }
-  // ***************************************************************************
+        {
+            static_cast<unsigned int>(iterations),
+            static_cast<double>(residual)
+        };
+}
+// ***************************************************************************
 }
 #endif
