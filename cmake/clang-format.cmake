1a3dbb53395ef720f01c693472b03d5d507138ad
fabio.vicini <fabio.vicini@polito.it>
2024-12-17 16:22:35 +0100

    still fix

--- cmake/clang-format.cmake
+++ cmake/clang-format.cmake
@@ -5,6 +5,10 @@ set(ENABLE_CLANGFORMAT OFF CACHE BOOL "Enable clangformat")
 set(CLANGFORMAT_STYLE "{BasedOnStyle: Microsoft, Language: Cpp, Standard: Cpp11, PointerAlignment: Left, DerivePointerAlignment: false, TabWidth: 4, IndentWidth: 4, UseTab: Always, AlignAfterOpenBracket: true, BreakBeforeBraces: Custom, SortUsingDeclarations: true, SortIncludes: true}")
 
 if(${ENABLE_CLANGFORMAT})
+
+	MESSAGE(WARNING ${CLANGFORMAT_STYLE})
+	MESSAGE(WARNING ${CLANGFORMAT_FILES})
+
 	add_custom_target(
 		clangformat
 		ALL
