815026dc24c161106ebb1ed0d88652e79c9f1001
gioanateora <gioana.teora@polito.it>
2024-12-18 12:28:03 +0100

    add polygon_mass

--- cmake/clang-format.cmake
+++ cmake/clang-format.cmake
@@ -2,7 +2,7 @@ file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.h)
 
 set(ENABLE_CLANGFORMAT OFF CACHE BOOL "Enable clangformat")
 
-set(CLANGFORMAT_STYLE "{BasedOnStyle: Microsoft, Language: Cpp, Standard: Cpp11, PointerAlignment: Left, DerivePointerAlignment: false, TabWidth: 4, IndentWidth: 4, UseTab: Always, AlignAfterOpenBracket: true, BreakBeforeBraces: Custom, SortUsingDeclarations: true, SortIncludes: true}")
+set(CLANGFORMAT_STYLE "{BasedOnStyle: Microsoft, AlignAfterOpenBracket: Align, BinPackArguments: 'false', BinPackParameters: 'false'}")
 
 if(${ENABLE_CLANGFORMAT})
 	add_custom_target(
