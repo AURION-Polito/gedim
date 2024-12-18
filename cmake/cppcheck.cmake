cf9c29fa9c16b1d9c022a00413395e60d0868bc3
fabio.vicini <fabio.vicini@polito.it>
2024-12-18 14:51:38 +0100

    add: filtert

--- cmake/cppcheck.cmake
+++ cmake/cppcheck.cmake
@@ -3,6 +3,8 @@ file(GLOB_RECURSE ALL_SOURCE_FILES src/*.c src/*.h)
 set(ENABLE_CPPCHECK OFF CACHE BOOL "Enable cppcheck")
 
 if(${ENABLE_CPPCHECK})
+	list(FILTER CPPCHECK_FILES INCLUDE REGEX ".cpp|.hpp")
+
 	add_custom_target(
 		    cppcheck 
 		    ALL
