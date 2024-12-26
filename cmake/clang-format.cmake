c927b8e087450e9efd156341736a50f13a5ebd7a
gioanateora <gioana.teora@polito.it>
2024-12-26 18:23:13 +0100

    fix clang format

--- cmake/clang-format.cmake
+++ cmake/clang-format.cmake
@@ -1,6 +1,6 @@
 set(ENABLE_CLANGFORMAT OFF CACHE BOOL "Enable clangformat")
 
-set(CLANGFORMAT_STYLE "{BasedOnStyle: Microsoft, AlignAfterOpenBracket: Align, BinPackArguments: 'false', BinPackParameters: 'false'}")
+set(CLANGFORMAT_STYLE "{BasedOnStyle: Microsoft, AllowAllArgumentsOnNextLine: 'false', AllowAllParametersOfDeclarationOnNextLine: 'false', AlwaysBreakAfterDefinitionReturnType: None, AlwaysBreakAfterReturnType: None, BinPackArguments: 'false', BinPackParameters: 'false', ColumnLimit: '120', PenaltyBreakAssignment: '5', PenaltyBreakBeforeFirstCallParameter: '20', PenaltyBreakComment: '0', PenaltyBreakFirstLessLess: '1', PenaltyBreakString: '5', PenaltyExcessCharacter: '1', PenaltyReturnTypeOnItsOwnLine: '150'}")
 
 if(${ENABLE_CLANGFORMAT})
 
@@ -12,6 +12,6 @@ if(${ENABLE_CLANGFORMAT})
 		COMMAND clang-format
 		-style=${CLANGFORMAT_STYLE}
 		-i
-		${CLANGFORMAT_FILES}
+                    ${CLANGFORMAT_FILES}
 	)
 endif()
