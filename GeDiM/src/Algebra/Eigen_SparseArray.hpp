fcf5651ef8599ac200c4c477feb8b3962064f2ca
Fabio Vicini <90675116+fvicini@users.noreply.github.com>
2025-05-21 15:17:09 +0200

    add: license on files (#153)
    * add: license on files
    
    * add: license on test

--- GeDiM/src/Algebra/Eigen_SparseArray.hpp
+++ GeDiM/src/Algebra/Eigen_SparseArray.hpp
@@ -1,3 +1,14 @@
+// _LICENSE_HEADER_
+//
+// Copyright (C) 2019 - 2025.
+// Terms register on the GPL-3.0 license.
+//
+// This file can be redistributed and/or modified under the license terms.
+//
+// See top level LICENSE file for more details.
+//
+// This file can be used citing references in CITATION.cff file.
+
 #ifndef __Eigen_SparseArray_HPP
 #define __Eigen_SparseArray_HPP
 
--- GeDiM/src/Quadrature/Quadrature_Gauss3D_Tetrahedron_PositiveWeights.hpp
+++ GeDiM/src/Quadrature/Quadrature_Gauss3D_Tetrahedron_PositiveWeights.hpp
@@ -1,3 +1,14 @@
+// _LICENSE_HEADER_
+//
+// Copyright (C) 2019 - 2025.
+// Terms register on the GPL-3.0 license.
+//
+// This file can be redistributed and/or modified under the license terms.
+//
+// See top level LICENSE file for more details.
+//
+// This file can be used citing references in CITATION.cff file.
+
 #ifndef __Quadrature_Gauss3D_Tetrahedron_PositiveWeights_H
 #define __Quadrature_Gauss3D_Tetrahedron_PositiveWeights_H
 
