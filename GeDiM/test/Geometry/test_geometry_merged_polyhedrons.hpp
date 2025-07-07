1612b3d1417eb2180656acf8400d490a8f263e6e
fabio.vicini <fabio.vicini@polito.it>
2025-07-07 11:17:09 +0200

    add: tests

--- GeDiM/test/Geometry/test_geometry_merged_polyhedrons.hpp
+++ GeDiM/test/Geometry/test_geometry_merged_polyhedrons.hpp
@@ -54,16 +54,7 @@ namespace GedimUnitTesting
     geometryUtilities.ExportPolyhedronToVTU(merged_polyhedron.MergedPolyhedron,
                                             exportFolder + "/merged_polyhedron");
 
-    for (unsigned int p = 0; p < polyhedrons.size(); ++p)
-    {
-      const auto& polyhedron = polyhedrons[p];
-      ASSERT_EQ(merged_polyhedron.OriginalToMergedVertices[p].size(),
-                polyhedron.Vertices.cols());
-      ASSERT_EQ(merged_polyhedron.OriginalToMergedEdges[p].size(),
-                polyhedron.Edges.cols());
-      ASSERT_EQ(merged_polyhedron.OriginalToMergedFaces[p].size(),
-                polyhedron.Faces.size());
-    }
+
     ASSERT_EQ(merged_polyhedron.MergedToOriginalVertices.size(),
               polyhedrons[0].Vertices.cols() +
         polyhedrons[1].Vertices.cols());
@@ -76,12 +67,51 @@ namespace GedimUnitTesting
     ASSERT_EQ(merged_polyhedron.MergedPolyhedron.Vertices.cols(),
               polyhedrons[0].Vertices.cols() +
         polyhedrons[1].Vertices.cols());
-    ASSERT_EQ(merged_polyhedron.MergedPolyhedron.Edges.size(),
+    ASSERT_EQ(merged_polyhedron.MergedPolyhedron.Edges.cols(),
               polyhedrons[0].Edges.cols() +
         polyhedrons[1].Edges.cols());
     ASSERT_EQ(merged_polyhedron.MergedPolyhedron.Faces.size(),
               polyhedrons[0].Faces.size() +
         polyhedrons[1].Faces.size());
+    for (unsigned int p = 0; p < polyhedrons.size(); ++p)
+    {
+      const auto& polyhedron = polyhedrons[p];
+      ASSERT_EQ(merged_polyhedron.OriginalToMergedVertices[p].size(),
+                polyhedron.Vertices.cols());
+      ASSERT_EQ(merged_polyhedron.OriginalToMergedEdges[p].size(),
+                polyhedron.Edges.cols());
+      ASSERT_EQ(merged_polyhedron.OriginalToMergedFaces[p].size(),
+                polyhedron.Faces.size());
+
+      const unsigned int shift_vertices = (p == 0) ? 0 :
+                                                     polyhedrons[p - 1].Vertices.cols();
+      const unsigned int shift_edges = (p == 0) ? 0 :
+                                                     polyhedrons[p - 1].Edges.cols();
+      const unsigned int shift_faces = (p == 0) ? 0 :
+                                                     polyhedrons[p - 1].Faces.size();
+
+      for (unsigned int v = 0; v < polyhedron.Vertices.cols(); ++v)
+      {
+        ASSERT_EQ(shift_vertices + v,
+                  merged_polyhedron.OriginalToMergedVertices[p][v]);
+        ASSERT_EQ(v,
+                  merged_polyhedron.MergedToOriginalVertices[shift_vertices + v]);
+      }
+      for (unsigned int e = 0; e < polyhedron.Edges.cols(); ++e)
+      {
+        ASSERT_EQ(shift_edges + e,
+                  merged_polyhedron.OriginalToMergedEdges[p][e]);
+        ASSERT_EQ(e,
+                  merged_polyhedron.MergedToOriginalEdges[shift_edges + e]);
+      }
+      for (unsigned int f = 0; f < polyhedron.Faces.size(); ++f)
+      {
+        ASSERT_EQ(shift_faces + f,
+                  merged_polyhedron.OriginalToMergedFaces[p][f]);
+        ASSERT_EQ(f,
+                  merged_polyhedron.MergedToOriginalFaces[shift_faces + f]);
+      }
+    }
   }
 
 } // namespace GedimUnitTesting
