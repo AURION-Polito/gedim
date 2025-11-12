557deb9e53483f7b2d62ce8cad5df05cbbf6c5d5
fabio.vicini <fabio.vicini@polito.it>
2025-11-12 15:21:19 +0100

    add: export of triangle mesh

--- GeDiM/src/Mesh/TriangleInterface.hpp
+++ GeDiM/src/Mesh/TriangleInterface.hpp
@@ -36,6 +36,9 @@ class TriangleInterface final
                               struct triangulateio &triangleOutput,
                               const std::string &triangleOptions = "-QDzpqnea") const;
 
+    void ConvertTriangleMeshToGedimMesh(struct triangulateio &triangleOutput,
+                                        IMeshDAO &mesh) const;
+
     void DeleteTriangleStructure(struct triangulateio &triangleInput, struct triangulateio &triangleOutput) const;
 
     void ExportTriangleMesh(const struct triangulateio &triangleInput,
@@ -46,6 +49,12 @@ class TriangleInterface final
   public:
     TriangleInterface();
     ~TriangleInterface();
+
+    void ExportMesh(const Eigen::MatrixXd &polygonVertices,
+                    const double &maxTriangleArea,
+                    const std::string& folder_name,
+                    const std::string& file_name,
+                    const std::string &triangleOptions = "-QDzpqnea") const;
 
     void CreateMesh(const Eigen::MatrixXd &polygonVertices,
                     const double &maxTriangleArea,
